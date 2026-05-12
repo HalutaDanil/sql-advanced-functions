92b963c410ece37f611cb12782e66ed717f09900
HalutaDanil <halutadanil@icloud.com>
2026-05-12 17:07:47 +0300

    chore: clean up school files and prepare for GitHub
    - Remove School 21 LICENSE and task descriptions
    - Remove empty template directories
    - Update author information to HalutaDanil
    - Add new project README
    - Rename branch to main

--- src/ex02/day09_ex02.sql
+++ src/ex02/day09_ex02.sql
@@ -1,32 +1,34 @@
-create function fnc_trg_person_delete_audit()
-returns trigger as
-$$
-begin
-insert into person_audit(
-        row_id,
-        name,
-        age,
-        gender,
-        address,
-        type_event
-    )
-    values (
-    OLD.id,
-        OLD.name,
-        OLD.age,
-        OLD.gender,
-        OLD.address,
-        'D'
-  );
-
-return OLD;
-end;
-$$ language plpgsql;
-
-create trigger trg_person_delete_audit
-after delete on person
-for each row
-execute function fnc_trg_person_delete_audit();
-
-delete from person
-where id = 10;
+-- create function fnc_trg_person_delete_audit()
+-- returns trigger as
+-- $$
+-- begin
+-- insert into person_audit(
+--         row_id,
+--         name,
+--         age,
+--         gender,
+--         address,
+--         type_event
+--     )
+--     values (
+--     OLD.id,
+--         OLD.name,
+--         OLD.age,
+--         OLD.gender,
+--         OLD.address,
+--         'D'
+--   );
+--
+-- return OLD;
+-- end;
+-- $$ language plpgsql;
+--
+-- create trigger trg_person_delete_audit
+-- after delete on person
+-- for each row
+-- execute function fnc_trg_person_delete_audit();
+--
+-- delete from person
+-- where id = 10;
+SELECT * FROM person_audit
+ORDER BY created;
--- src/ex03/day09_ex03.sql
+++ src/ex03/day09_ex03.sql
@@ -1,96 +1,99 @@
-drop trigger trg_person_insert_audit on person;
-drop function fnc_trg_person_insert_audit();
+-- drop trigger trg_person_insert_audit on person;
+-- drop function fnc_trg_person_insert_audit();
+--
+-- drop trigger trg_person_update_audit on person;
+-- drop function fnc_trg_person_update_audit();
+--
+-- drop trigger trg_person_delete_audit on person;
+-- drop function fnc_trg_person_delete_audit();
+--
+-- truncate table person_audit;
+--
+-- create function fnc_trg_person_audit()
+-- returns trigger as
+-- $$
+-- begin
+--     if tg_op = 'DELETE' then
+--         insert into person_audit(
+--             row_id,
+--             name,
+--             age,
+--             gender,
+--             address,
+--             type_event
+--         )
+--         values (
+--             OLD.id,
+--             OLD.name,
+--             OLD.age,
+--             OLD.gender,
+--             OLD.address,
+--             'D'
+--         );
+--
+--         return OLD;
+--
+--     elsif tg_op = 'UPDATE' then
+--         insert into person_audit(
+--             row_id,
+--             name,
+--             age,
+--             gender,
+--             address,
+--             type_event
+--         )
+--         values (
+--             OLD.id,
+--             OLD.name,
+--             OLD.age,
+--             OLD.gender,
+--             OLD.address,
+--             'U'
+--         );
+--
+--         return NEW;
+--
+--     elsif tg_op = 'INSERT' then
+--         insert into person_audit(
+--             row_id,
+--             name,
+--             age,
+--             gender,
+--             address
+--         )
+--         values (
+--             NEW.id,
+--             NEW.name,
+--             NEW.age,
+--             NEW.gender,
+--             NEW.address
+--         );
+--
+--         return NEW;
+--     end if;
+--
+--     return null;
+-- end;
+-- $$ language plpgsql;
+--
+-- create trigger trg_person_audit
+-- after insert or update or delete on person
+-- for each row
+-- execute function fnc_trg_person_audit();
+--
+-- insert into person (id, name, age, gender, address)
+-- values (10, 'Damir', 22, 'male', 'Irkutsk');
+--
+-- update person
+-- set name = 'Bulat'
+-- where id = 10;
+--
+-- update person
+-- set name = 'Damir'
+-- where id = 10;
+--
+-- delete from person
+-- where id = 10;
 
-drop trigger trg_person_update_audit on person;
-drop function fnc_trg_person_update_audit();
-
-drop trigger trg_person_delete_audit on person;
-drop function fnc_trg_person_delete_audit();
-
-truncate table person_audit;
-
-create function fnc_trg_person_audit()
-returns trigger as
-$$
-begin
-    if tg_op = 'DELETE' then
-        insert into person_audit(
-            row_id,
-            name,
-            age,
-            gender,
-            address,
-            type_event
-        )
-        values (
-            OLD.id,
-            OLD.name,
-            OLD.age,
-            OLD.gender,
-            OLD.address,
-            'D'
-        );
-
-        return OLD;
-
-    elsif tg_op = 'UPDATE' then
-        insert into person_audit(
-            row_id,
-            name,
-            age,
-            gender,
-            address,
-            type_event
-        )
-        values (
-            OLD.id,
-            OLD.name,
-            OLD.age,
-            OLD.gender,
-            OLD.address,
-            'U'
-        );
-
-        return NEW;
-
-    elsif tg_op = 'INSERT' then
-        insert into person_audit(
-            row_id,
-            name,
-            age,
-            gender,
-            address
-        )
-        values (
-            NEW.id,
-            NEW.name,
-            NEW.age,
-            NEW.gender,
-            NEW.address
-        );
-
-        return NEW;
-    end if;
-
-    return null;
-end;
-$$ language plpgsql;
-
-create trigger trg_person_audit
-after insert or update or delete on person
-for each row
-execute function fnc_trg_person_audit();
-
-insert into person (id, name, age, gender, address)
-values (10, 'Damir', 22, 'male', 'Irkutsk');
-
-update person
-set name = 'Bulat'
-where id = 10;
-
-update person
-set name = 'Damir'
-where id = 10;
-
-delete from person
-where id = 10;
+SELECT * FROM person_audit
+ORDER BY created;
